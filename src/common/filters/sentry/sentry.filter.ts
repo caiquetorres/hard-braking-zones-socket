e9a42b94e331de46176d6812b5a7a08435da73dc
caiquetorres <caiquetorres.bastos@hotmail.com>
2022-01-22 18:11:31 -0300

    fix(sentry.filter.ts): removed the unused "http" logic from the filter

--- src/common/filters/sentry/sentry.filter.ts
+++ src/common/filters/sentry/sentry.filter.ts
@@ -1,32 +1,24 @@
-import {
-  ArgumentsHost,
-  Catch,
-  ExceptionFilter,
-  HttpException,
-  HttpStatus,
-  Logger,
-} from '@nestjs/common'
+import { ArgumentsHost, Catch, HttpException } from '@nestjs/common'
+import { BaseWsExceptionFilter } from '@nestjs/websockets'
 import * as Sentry from '@sentry/node'
 
 import { EnvService } from '../../../env/env.service'
-
-import { Request, Response } from 'express'
 
 /**
  * Class that represents the filter that capture some exception and send
  * it to sentry.io.
  */
 @Catch()
-export class SentryFilter implements ExceptionFilter {
+export class SentryFilter extends BaseWsExceptionFilter {
   /**
    * Property that defines if the sentry is enabled or not for this
    * application.
    */
   private readonly sentryEnabled: boolean
 
-  private readonly logger = new Logger(SentryFilter.name)
-
   constructor(envService: EnvService) {
+    super()
+
     this.sentryEnabled =
       envService.get('SENTRY_ENABLED') && !!envService.get('SENTRY_DSN')
 
@@ -47,29 +39,10 @@ export class SentryFilter implements ExceptionFilter {
    * @param host defines and object that represents the host arguments.
    */
   catch(exception: HttpException, host: ArgumentsHost): void {
-    const context = host.switchToHttp()
-    const response = context.getResponse<Response>()
-    const request = context.getRequest<Request>()
-
-    const isHttpException = exception instanceof HttpException
-
-    const status = isHttpException
-      ? exception.getStatus()
-      : HttpStatus.INTERNAL_SERVER_ERROR
-
-    if (status >= 500) {
-      this.logger.error(exception.name, exception.stack)
-
-      if (this.sentryEnabled) {
-        Sentry.captureException(exception)
-      }
+    if (this.sentryEnabled) {
+      Sentry.captureException(exception)
     }
 
-    response.status(status).json({
-      statusCode: status,
-      timestamp: new Date().toISOString(),
-      path: request.url,
-      message: isHttpException ? exception.message : 'Internal Server Error',
-    })
+    super.catch(exception, host)
   }
 }
