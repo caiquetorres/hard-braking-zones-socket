6581f92d91a2ac6534302bf1a9e1cb68f6b14885
caiquetorres <caiquetorres.bastos@hotmail.com>
2022-01-23 11:52:45 -0300

    build: removed "bull", "@nestjs/bull" and "@types/bull"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
-import { BullModule } from '@nestjs/bull'
 import { Module } from '@nestjs/common'
 
-import { BullConfigService } from './common/config/bull/bull-config.service'
 import { InfluxConfigService } from './common/config/influx/influx-config.service'
 
 import { EnvModule } from './env/env.module'
@@ -15,9 +13,6 @@ import { WsAdapterModule } from './ws-adapter/ws-adapter.module'
     WsAdapterModule,
     EnvModule.forRoot({
       envFilePath: ['.env'],
-    }),
-    BullModule.forRootAsync({
-      useClass: BullConfigService,
     }),
     InfluxModule.forRootAsync({
       useClass: InfluxConfigService,
